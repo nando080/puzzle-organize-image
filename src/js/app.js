488830e17bb1bc162be01306245ad23ea6467f2f
nando080 <nando080@hotmail.com>
2026-04-10 10:37:02 -0300

    Funcionalidade visual do seletor de dificuldade

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-console.log('teste')
+const difficultySelector = document.querySelector('.options__toggle')
+
+difficultySelector.addEventListener('click', () => {
+    const isEasy = difficultySelector.classList.contains('options__toggle--easy')
+    const isRegular = difficultySelector.classList.contains('options__toggle--regular')
+    difficultySelector.classList.remove('options__toggle--easy', 'options__toggle--regular')
+    if (isEasy) {
+        difficultySelector.classList.add('options__toggle--regular')
+    }
+    if (isRegular) {
+    difficultySelector.classList.add('options__toggle--easy')
+    }
+})
+
+console.log(difficultySelector)
